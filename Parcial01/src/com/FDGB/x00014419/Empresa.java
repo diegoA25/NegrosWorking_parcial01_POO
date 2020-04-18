1a9e2a35918884c64b05ca4013ad4d2cc19938fe
diegoA25 <00044919@uca.edu.sv>
2020-04-17 21:38:20 -0600

    Empresa

---
+++ Parcial01/src/com/FDGB/x00014419/Empresa.java
@@ -0,0 +1,30 @@
+package com.DEAC.x00044919;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empresa{
+    private String nombre;
+    private List<Empleado> planilla = new ArrayList<Empleado>();
+
+    //Constructor
+    public Empresa(String nombre){
+        this.nombre = nombre;
+    }
+
+    //Setters & Getters
+    public String getNombre(){
+        return nombre;
+    }
+    public List<Empleado> getPlanilla(){
+        return planilla;
+    }
+
+    //Metodos
+    public void addEmpleado(Empleado empleado){
+
+    }
+    public void quitEmpleado(String nombre){
+
+    }
+}
